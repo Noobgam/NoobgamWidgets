d7bf21d202c6b040456f9ad916e7d4e9795e1c31
Noobgam <shessmaster12@gmail.com>
2025-01-04 23:55:22 +0100

    chore: refactor + option to hide the window

--- src/renderer.ts
+++ src/renderer.ts
@@ -29,4 +29,4 @@
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');// Add this to the end of the existing file
-import './App';
+import './components/App';
